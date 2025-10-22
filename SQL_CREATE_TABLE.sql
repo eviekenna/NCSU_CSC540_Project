5a6a1ba9f045b767b4f8ce5c1535b00b7059c5d5
Will Carter <157166314+willctr@users.noreply.github.com>
2025-10-22 12:34:44 -0400

    Update SQL_CREATE_TABLE.sql
    add user table, add SupplierFormulationMaterials table, add constraints, add drops, type changes

--- SQL_CREATE_TABLE.sql
+++ SQL_CREATE_TABLE.sql
@@ -5,6 +5,7 @@
 
   DROP TABLE IF EXISTS BatchConsumption;
   DROP TABLE IF EXISTS DoNotCombine;
+  DROP TABLE IF EXISTS SupplierFormulationMaterials;
   DROP TABLE IF EXISTS SupplierFormulation;
   DROP TABLE IF EXISTS IngredientBatch;
   DROP TABLE IF EXISTS ProductBatch;
@@ -15,7 +16,15 @@
   DROP TABLE IF EXISTS Manufacturer;
   DROP TABLE IF EXISTS Ingredient;
   DROP TABLE IF EXISTS Category;
+  DROP TABLE IF EXISTS User;
 
+
+  CREATE TABLE User (
+    user_id VARCHAR(100) PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    role_code ENUM('MANUFACTURER', 'SUPPLIER', 'VIEWER') NOT NULL
+  );
 
   CREATE TABLE Category (
     category_id       INT UNSIGNED PRIMARY KEY,
@@ -30,20 +39,23 @@
   );
 
   CREATE TABLE Manufacturer (
-    manufacturer_id INT PRIMARY KEY, 
-    manufacturer_name VARCHAR(100)
+    manufacturer_id VARCHAR(100) PRIMARY KEY, 
+    manufacturer_name VARCHAR(100),
+    CONSTRAINT manf_user_fk FOREIGN KEY (manufacturer_id) REFERENCES User(user_id)
   );
 
   CREATE TABLE Supplier (
-    supplier_id INT PRIMARY KEY,
-    supplier_name VARCHAR(100) NOT NULL
+    supplier_id VARCHAR(100) PRIMARY KEY,
+    supplier_name VARCHAR(100) NOT NULL,
+    CONSTRAINT sup_user_fk FOREIGN KEY (supplier_id) REFERENCES User(user_id)
   );
     
   CREATE TABLE Product (
     product_id INT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-    manufacturer_id INT NOT NULL,
+    manufacturer_id VARCHAR(100) NOT NULL,
     category_id INT UNSIGNED NOT NULL,
+    standard_batch_units INT NOT NULL,
     CONSTRAINT prod_manufacturer_id_fk FOREIGN KEY (manufacturer_id) REFERENCES Manufacturer(manufacturer_id),
     CONSTRAINT prod_category_id_fk FOREIGN KEY (category_id) REFERENCES Category(category_id)
   );
@@ -58,16 +70,18 @@
   CREATE TABLE IngredientComposition (
     parent_ingredient_id INT,
     child_ingredient_id INT,
-    quantity INT,
+    quantity DECIMAL(10, 2) NOT NULL CHECK (quantity > 0),
     PRIMARY KEY (parent_ingredient_id, child_ingredient_id),
     CONSTRAINT parent_ingredient_id_fk FOREIGN KEY (parent_ingredient_id) REFERENCES Ingredient(ingredient_id),
-    CONSTRAINT child_ingredient_id_fk FOREIGN KEY (child_ingredient_id) REFERENCES Ingredient(ingredient_id)
+    CONSTRAINT child_ingredient_id_fk FOREIGN KEY (child_ingredient_id) REFERENCES Ingredient(ingredient_id),
+    CONSTRAINT parent_notequal_child CHECK (parent_ingredient_id != child_ingredient_id)
   );
     
+  -- product BOM 
   CREATE TABLE Recipe (
     product_id INT,
     ingredient_id INT,
-    quantity DECIMAL(12, 2) NOT NULL,
+    quantity DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY (product_id, ingredient_id),
     CONSTRAINT recipe_product_id_fk FOREIGN KEY (product_id) REFERENCES Product(product_id),
     CONSTRAINT recipe_ingredient_id_fk FOREIGN KEY (ingredient_id) REFERENCES Ingredient(ingredient_id)
@@ -78,10 +92,10 @@
   CREATE TABLE ProductBatch (
     batch_id INT AUTO_INCREMENT PRIMARY KEY,
     product_id INT NOT NULL,
-    manufacturer_id INT NOT NULL,
+    manufacturer_id VARCHAR(100) NOT NULL,
     lot_number VARCHAR(100) UNIQUE,
     quantity INT NOT NULL CHECK (quantity >= 0),
-    cost INT NOT NULL CHECK (cost >= 0),
+    unit_cost DECIMAL(10, 2) NOT NULL CHECK (unit_cost >= 0),
     production_date DATE NOT NULL DEFAULT CURRENT_DATE, -- trace product for recalls
     expiration_date DATE NOT NULL,
     CONSTRAINT batch_product_id_fk FOREIGN KEY (product_id) REFERENCES Product(product_id),
@@ -92,10 +106,11 @@
   CREATE TABLE IngredientBatch (
     batch_id INT AUTO_INCREMENT PRIMARY KEY,
     ingredient_id INT NOT NULL,
-    supplier_id INT NOT NULL,
+    supplier_id VARCHAR(100) NOT NULL,
     lot_number VARCHAR(100) UNIQUE,
-    quantity_oz INT NOT NULL CHECK (quantity_oz >= 0),
-    cost DECIMAL(12, 2) NOT NULL CHECK (cost >= 0),
+    quantity_oz DECIMAL(10, 2) NOT NULL CHECK (quantity_oz >= 0),
+    available_oz DECIMAL(10, 2) NOT NULL CHECK (available_oz >= 0),
+    unit_cost DECIMAL(10, 2) NOT NULL CHECK (unit_cost >= 0),
     expiration_date DATE NOT NULL,
     intake_date DATE NOT NULL DEFAULT CURRENT_DATE,
     CONSTRAINT batch_supplier_id_fk FOREIGN KEY (supplier_id) REFERENCES Supplier(supplier_id),
@@ -103,22 +118,39 @@
     CONSTRAINT check_90_day_minimum CHECK (DATEDIFF(expiration_date, intake_date) >= 90)
   );
 
+  -- ingredient formulation
   CREATE TABLE SupplierFormulation (
     formulation_id INT AUTO_INCREMENT PRIMARY KEY,
-    supplier_id INT NOT NULL,
+    supplier_id VARCHAR(100) NOT NULL,
     ingredient_id INT NOT NULL,
-    pack_size INT NOT NULL CHECK (pack_size > 0),
-    price_per_unit DECIMAL(12, 2) NOT NULL CHECK (price_per_unit >= 0),
+    version_no INT NOT NULL,
+    pack_size DECIMAL(10, 2) NOT NULL CHECK (pack_size > 0),
+    price_per_unit DECIMAL(10, 2) NOT NULL CHECK (price_per_unit >= 0),
     effective_period_start_date DATE NOT NULL,
     effective_period_end_date DATE,
+    UNIQUE (supplier_id, ingredient_id, version_no), -- supplier version of ingredients should be unique
     CONSTRAINT form_supplier_id_fk FOREIGN KEY (supplier_id) REFERENCES Supplier(supplier_id),
-    CONSTRAINT form_ingredient_id_fk FOREIGN KEY (ingredient_id) REFERENCES Ingredient(ingredient_id)
+    CONSTRAINT form_ingredient_id_fk FOREIGN KEY (ingredient_id) REFERENCES Ingredient(ingredient_id),
+    CONSTRAINT date_order CHECK (effective_period_end_date IS NULL OR effective_period_start_date < effective_period_end_date) -- doesn't expire or start date is before end date
+  );
+
+  /** different suppliers can have different formulations 
+    i.e. supplier A seasoning blend is different from supplier B seasoning blend
+  */
+  CREATE TABLE SupplierFormulationMaterials (
+    formulation_id INT NOT NULL,
+    ingredient_id INT NOT NULL,
+    qty DECIMAL(10, 2) NOT NULL CHECK (qty > 0),
+    PRIMARY KEY (formulation_id, ingredient_id), 
+    CONSTRAINT supmat_formulation_id_fk FOREIGN KEY (formulation_id) REFERENCES SupplierFormulation(formulation_id),
+    CONSTRAINT supmat_ingredient_fk FOREIGN KEY (ingredient_id) REFERENCES Ingredient(ingredient_id)
   );
 
   CREATE TABLE BatchConsumption (
     product_lot_number VARCHAR(100) NOT NULL,
     ingredient_lot_number VARCHAR(100) NOT NULL,
-    quantity_consumed INT NOT NULL,
+    quantity_consumed DECIMAL(10, 2) NOT NULL,
+    consumption_date DATE NOT NULL DEFAULT CURRENT_DATE, -- for recall 20 day window
     PRIMARY KEY (product_lot_number, ingredient_lot_number),
     CONSTRAINT bcons_product_lot_number_fk FOREIGN KEY (product_lot_number) REFERENCES ProductBatch(lot_number),
     CONSTRAINT bcons_ingredient_lot_number_fk FOREIGN KEY (ingredient_lot_number) REFERENCES IngredientBatch(lot_number)
@@ -179,5 +211,4 @@
   
   
   /* Populate tables with Insert */
-  
   
